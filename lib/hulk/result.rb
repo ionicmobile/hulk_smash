93ce790fc41e41cafa851c1203bbc1ffa469af25
Matt Simpson <matt.simpson@asolutions.com>
2012-08-01 13:45:54 -0500

    Validate the siege result to make sure the user is using Siege version 2.x

--- lib/hulk/result.rb
+++ lib/hulk/result.rb
@@ -1,27 +1,48 @@
 module Hulk
   class Result
-    attr_reader :siege_result, :avg_response_time, :requests_per_second
+    attr_reader :siege_result, :avg_response_time, :requests_per_second, :errors
 
     def initialize(siege_result)
       @siege_result = siege_result
       parse_result
     end
 
+    def valid?
+      @errors = []
+      validate_version_is_supported
+      errors.empty?
+    end
+
+    def reasons_for_failure
+      errors.join(", ")
+    end
+
     private
 
     def parse_result
-      parse_avg_response_time
-      parse_requests_per_second
+      if valid?
+        parse_avg_response_time
+        parse_requests_per_second
+      end
+    end
+
+    def version
+      regex = /\*\* SIEGE \s*([\d\.]*)$/
+      @version ||= regex.match(siege_result)[1].to_f
     end
 
     def parse_avg_response_time
-      regex = /^Response time:\s*([\d\.]*) secs$/
+      regex = /Response time:\s*([\d\.]*) secs/
       @avg_response_time = regex.match(siege_result)[1].to_f
     end
 
     def parse_requests_per_second
-      regex = /^Transaction rate:\s*([\d\.]*) trans\/sec$/
+      regex = /Transaction rate:\s*([\d\.]*) trans\/sec/
       @requests_per_second = regex.match(siege_result)[1].to_f
-    end    
+    end
+
+    def validate_version_is_supported
+      @errors << "Siege version must be 2.x" if version < 2 || version >= 3
+    end
   end
 end
--- spec/lib/hulk/result_spec.rb
+++ spec/lib/hulk/result_spec.rb
@@ -3,33 +3,52 @@ require_relative '../../../lib/hulk/result'
 describe Hulk::Result do
   subject { described_class.new siege_result }
 
-  let(:siege_result) do 
-%(
-** SIEGE 2.71
-** Preparing 15 concurrent users for battle.
-The server is now under siege...
-Lifting the server siege...      done.
+  context 'when the siege results is a complete and valid result' do
+    let(:siege_result) do 
+  %(
+  ** SIEGE 2.71
+  ** Preparing 15 concurrent users for battle.
+  The server is now under siege...
+  Lifting the server siege...      done.
 
-Transactions:		       12487 hits
-Availability:		      100.00 %
-Elapsed time:		        9.41 secs
-Data transferred:	       32.19 MB
-Response time:		        0.01 secs
-Transaction rate:	     1326.99 trans/sec
-Throughput:		        3.42 MB/sec
-Concurrency:		       14.97
-Successful transactions:       12487
-Failed transactions:	           0
-Longest transaction:	        0.03
-Shortest transaction:	        0.00
-)
+  Transactions:		       12487 hits
+  Availability:		      100.00 %
+  Elapsed time:		        9.41 secs
+  Data transferred:	       32.19 MB
+  Response time:		        0.01 secs
+  Transaction rate:	     1326.99 trans/sec
+  Throughput:		        3.42 MB/sec
+  Concurrency:		       14.97
+  Successful transactions:       12487
+  Failed transactions:	           0
+  Longest transaction:	        0.03
+  Shortest transaction:	        0.00
+  )
+    end
+
+    it 'has an average response time' do
+      subject.avg_response_time.should == 0.01
+    end
+
+    it 'has the rate of requests per second' do
+      subject.requests_per_second.should == 1326.99
+    end
   end
 
-  it 'has an average response time' do
-    subject.avg_response_time.should == 0.01
-  end
+  context 'when the siege results is not from siege version 2' do
+    let(:siege_result) do 
+  %(
+  ** SIEGE 1.71
+  jibber jabber
+  )
+    end
 
-  it 'has the rate of requests per second' do
-    subject.requests_per_second.should == 1326.99
+    it 'is invalid' do
+      subject.should_not be_valid
+    end
+
+    it 'contains reasons for the failure' do
+      subject.reasons_for_failure.should include("Siege version must be 2.x")
+    end
   end
 end
