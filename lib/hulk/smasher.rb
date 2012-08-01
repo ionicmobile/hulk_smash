d2d40ff0bb41deb62891f17f7d573539d9f8b7a7
Matt Simpson <matt.simpson@asolutions.com>
2012-08-01 16:35:40 -0500

    Allow changing the default duration

--- lib/hulk/smasher.rb
+++ lib/hulk/smasher.rb
@@ -3,21 +3,27 @@ require 'stringio'
 
 module Hulk
   class Smasher
-    attr_reader :url
+    attr_reader :url, :duration
 
-    def initialize(url)
+    def initialize(url='http://localhost', options={})
+      options = default_options.merge(options)
+      @duration = options[:duration]
       @url = url
     end
 
     def run_load_test
-      execute_siege_command "siege -t5s -b #{url}"
+      execute_siege_command "siege -t#{duration} -b #{url}"
     end
 
     def run_scalability_test
-      execute_siege_command "siege -t5s #{url}"
+      execute_siege_command "siege -t#{duration} #{url}"
     end
 
     private
+
+    def default_options
+      { duration: '5s' }
+    end
 
     def execute_siege_command(command)
       `#{command} > #{results_file} 2>&1`
--- spec/lib/hulk/smasher_spec.rb
+++ spec/lib/hulk/smasher_spec.rb
@@ -1,8 +1,49 @@
 require_relative '../../../lib/hulk/smasher'
 
-describe Hulk::Smasher do
-  subject { described_class.new url }
+describe Hulk::Smasher, 'with default options:' do
+  let(:hulk_result) { mock 'parsed siege results used by hulk' }
+  let(:siege_results_file) { File.expand_path('../../../../log/results.log', __FILE__) }
+  let(:siege_results_contents) { mock 'results' }
 
+  before do
+    subject.stub(:`)
+    File.stub(:read).with(siege_results_file).and_return(siege_results_contents)
+    Hulk::Result.stub(:new).with(siege_results_contents).and_return(hulk_result)
+  end
+
+  describe "Running a load test" do
+    it 'uses siege to run a load test against the given url' do
+      siege_command = "siege -t5s -b http://localhost > #{siege_results_file} 2>&1"
+      subject.should_receive(:`).with(siege_command)
+
+      subject.run_load_test
+    end
+
+    it 'returns the result of the load test' do
+      subject.run_load_test.should == hulk_result
+    end
+  end
+
+  describe "Running a load test" do
+    it 'uses siege to run a scalability test against the given url' do
+      siege_command = "siege -t5s http://localhost > #{siege_results_file} 2>&1"
+      subject.should_receive(:`).with(siege_command)
+
+      subject.run_scalability_test
+    end
+
+    it 'returns the result of the load test' do
+      Hulk::Result.should_receive(:new).with(siege_results_contents).and_return(hulk_result)
+
+      subject.run_scalability_test.should == hulk_result
+    end
+  end
+end
+
+describe Hulk::Smasher, 'with custom options:' do
+  subject { described_class.new url, duration: duration }
+
+  let(:duration) { '1s' }
   let(:url) { mock 'url to smash' }
   let(:hulk_result) { mock 'parsed siege results used by hulk' }
   let(:siege_results_file) { File.expand_path('../../../../log/results.log', __FILE__) }
@@ -16,7 +57,7 @@ describe Hulk::Smasher do
 
   describe "Running a load test" do
     it 'uses siege to run a load test against the given url' do
-      siege_command = "siege -t5s -b #{url} > #{siege_results_file} 2>&1"
+      siege_command = "siege -t1s -b #{url} > #{siege_results_file} 2>&1"
       subject.should_receive(:`).with(siege_command)
 
       subject.run_load_test
@@ -29,7 +70,7 @@ describe Hulk::Smasher do
 
   describe "Running a load test" do
     it 'uses siege to run a scalability test against the given url' do
-      siege_command = "siege -t5s #{url} > #{siege_results_file} 2>&1"
+      siege_command = "siege -t1s #{url} > #{siege_results_file} 2>&1"
       subject.should_receive(:`).with(siege_command)
 
       subject.run_scalability_test
